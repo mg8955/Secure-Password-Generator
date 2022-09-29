42e4296e9e68ba35acb3bc4e6e84f7cb832d277a
Michael Gostomski <gostomski.m@gmail.com>
2022-09-29 15:06:50 -0500

    Final Update to JS

--- assets/script.js
+++ assets/script.js
@@ -19,39 +19,37 @@ function generatePassword() {
     console.log(inputLength);
   // Input validation begin - This is working
       if ((inputLength >= 8) && (inputLength <= 128)) {
-      var addSpChar = confirm("Please select OK to add special characters to your password.");
-        console.log(addSpChar);
-      var addNum = confirm("Please select OK to add numbers to your password.");
-        console.log(addNum);
-    } else {
+      } else {
       var quit = confirm("Invalid length. Please select OK to try again");
       if (quit === false) {
         return;
       } else {
         generatePassword();
-      }
-    }
+      }}
+      var addSpChar = confirm("Please select OK to add special characters to your password.");
+        console.log(addSpChar);
+      var addNum = confirm("Please select OK to add numbers to your password.");
+        console.log(addNum);
       if (addSpChar || addNum) {
-      var addUpp = confirm("Please select OK to add Uppercase letters to your password.");
-        console.log(addUpp);
-      var addLow = confirm("Please select OK to add Lowercase letters to your password.");
-        console.log(addLow);
-    } else {
+      } else {
       var quit = confirm("Please click OK to retry and add either Special Characters or Numbers to your password.");
         if (quit === false) {
         return;
       } else {
         generatePassword();
-      }
-    }
-      if (!addUpp && !addLow) {
-        var quit = confirm("Please click OK to retry and add either Upper Case or Lower Case Letters");
-        if (quit === false) {
+      }}
+      var addUpp = confirm("Please select OK to add Uppercase letters to your password.");
+        console.log(addUpp);
+      var addLow = confirm("Please select OK to add Lowercase letters to your password.");
+        console.log(addLow);
+      if (addUpp || addLow) {
+      } else {
+      var quit = confirm("Please click OK to retry and add either Upper Case or Lower Case Letters");
+      if (quit === false) {
           return;
-        } else {
-          generatePassword();
-        }
-      }
+      } else {
+        generatePassword();
+      }}
   // Input validation end - This is working
   // Password generation begin
   var pwdOutput = "";
@@ -63,7 +61,7 @@ function generatePassword() {
     emptyString: "",
   };
   if (addUpp && addSpChar && !addLow && !addNum) {
-    while (pwdOutput.length <= inputLength) {
+    while (pwdOutput.length < inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
@@ -71,15 +69,15 @@ function generatePassword() {
     console.log(pwdOutput);
     return pwdOutput;
   } else if (addUpp && addSpChar && addNum && !addLow) {
-    while (pwdOutput.length <= inputLength) {
-      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar, stringContainer.nums);
+    while (pwdOutput.length < inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.spChar, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
       console.log(pwdOutput);
       return pwdOutput;
   } else if (addUpp && addSpChar && !addNum && addLow) {
-    while (pwdOutput.length <= inputLength) {
+    while (pwdOutput.length < inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar, stringContainer.nums, stringContainer.loABC);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
@@ -87,39 +85,39 @@ function generatePassword() {
     console.log(pwdOutput);
     return pwdOutput;
   } else if (addUpp && !addSpChar && addNum && !addLow) {
-    while (pwdOutput.length <= inputLength) {
-      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums);
+    while (pwdOutput.length < inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
     console.log(pwdOutput);
     return pwdOutput;
   } else if (addUpp && addNum && !addSpChar && addLow) {
-    while (pwdOutput.length <= inputLength) {
-      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC);
+    while (pwdOutput.length < inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
     console.log(pwdOutput);
     return pwdOutput;
   } else if (addUpp && addNum && addSpChar && addLow) {
-    while (pwdOutput.length <= inputLength) {
-      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC, stringContainer.spChar);
+    while (pwdOutput.length < inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC, stringContainer.spChar, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
     console.log(pwdOutput);
     return pwdOutput;
   } else if (!addUpp && addNum && !addSpChar && addLow){
-    while (pwdOutput.length <= inputLength) {
-      stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.nums);
+    while (pwdOutput.length < inputLength) {
+      stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.nums, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
     console.log(pwdOutput);
     return pwdOutput;
   } else if (!addUpp && !addNum && addSpChar && addLow){
-    while (pwdOutput.length <= inputLength) {
+    while (pwdOutput.length < inputLength) {
       stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.spChar);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
@@ -127,15 +125,14 @@ function generatePassword() {
     console.log(pwdOutput);
     return pwdOutput;
   } else {
-    while (pwdOutput.length <= inputLength) {
+    while (pwdOutput.length < inputLength) {
       stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.nums, stringContainer.spChar);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
     console.log(pwdOutput);
     return pwdOutput;
-  }
-} 
+  }} 
 
 // generatePassword();
 
