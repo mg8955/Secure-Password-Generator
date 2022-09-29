7f1ee257df9706102d6f7fb40368ef30dad960ba
Michael Gostomski <gostomski.m@gmail.com>
2022-09-29 12:47:07 -0500

    final update to JS code fully functioning.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
@@ -37,14 +37,23 @@ function generatePassword() {
       var addLow = confirm("Please select OK to add Lowercase letters to your password.");
         console.log(addLow);
     } else {
-        confirm("Please click OK to retry and add either Special Characters or Numbers to your password.");
+      var quit = confirm("Please click OK to retry and add either Special Characters or Numbers to your password.");
         if (quit === false) {
         return;
       } else {
         generatePassword();
       }
     }
+      if (!addUpp && !addLow) {
+        var quit = confirm("Please click OK to retry and add either Upper Case or Lower Case Letters");
+        if (quit === false) {
+          return;
+        } else {
+          generatePassword();
+        }
+      }
   // Input validation end - This is working
+  // Password generation begin
   var pwdOutput = "";
   var stringContainer = {
     upABC: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -54,88 +63,81 @@ function generatePassword() {
     emptyString: "",
   };
   if (addUpp && addSpChar && !addLow && !addNum) {
-    while (pwdOutput.length <= 256) {
+    while (pwdOutput.length <= inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     }
-    console.log(pwdOutput.slice(0, inputLength));
+    console.log(pwdOutput);
+    return pwdOutput;
   } else if (addUpp && addSpChar && addNum && !addLow) {
-    while (pwdOutput.length <= 256) {
+    while (pwdOutput.length <= inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar, stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
-      console.log(pwdOutput.slice(0, inputLength));
+      console.log(pwdOutput);
+      return pwdOutput;
   } else if (addUpp && addSpChar && !addNum && addLow) {
-    while (pwdOutput.length <= 256) {
+    while (pwdOutput.length <= inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.spChar, stringContainer.nums, stringContainer.loABC);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
-    console.log(pwdOutput.slice(0, inputLength));
+    console.log(pwdOutput);
+    return pwdOutput;
   } else if (addUpp && !addSpChar && addNum && !addLow) {
-    while (pwdOutput.length <= 256) {
+    while (pwdOutput.length <= inputLength) {
       stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums);
       console.log(stringContainer.emptyString);
       pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
     } 
-    console.log(pwdOutput.slice(0, inputLength));
+    console.log(pwdOutput);
+    return pwdOutput;
+  } else if (addUpp && addNum && !addSpChar && addLow) {
+    while (pwdOutput.length <= inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC);
+      console.log(stringContainer.emptyString);
+      pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
+    } 
+    console.log(pwdOutput);
+    return pwdOutput;
+  } else if (addUpp && addNum && addSpChar && addLow) {
+    while (pwdOutput.length <= inputLength) {
+      stringContainer.emptyString = stringContainer.upABC.concat(stringContainer.nums, stringContainer.loABC, stringContainer.spChar);
+      console.log(stringContainer.emptyString);
+      pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
+    } 
+    console.log(pwdOutput);
+    return pwdOutput;
+  } else if (!addUpp && addNum && !addSpChar && addLow){
+    while (pwdOutput.length <= inputLength) {
+      stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.nums);
+      console.log(stringContainer.emptyString);
+      pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
+    } 
+    console.log(pwdOutput);
+    return pwdOutput;
+  } else if (!addUpp && !addNum && addSpChar && addLow){
+    while (pwdOutput.length <= inputLength) {
+      stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.spChar);
+      console.log(stringContainer.emptyString);
+      pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
+    } 
+    console.log(pwdOutput);
+    return pwdOutput;
+  } else {
+    while (pwdOutput.length <= inputLength) {
+      stringContainer.emptyString = stringContainer.loABC.concat(stringContainer.nums, stringContainer.spChar);
+      console.log(stringContainer.emptyString);
+      pwdOutput += stringContainer.emptyString[Math.floor(Math.random() * stringContainer.emptyString.length)];
+    } 
+    console.log(pwdOutput);
+    return pwdOutput;
   }
-
-  // while (pwdOutput.length <= 256) {
-  //   pwdOutput += stringContainer.upABC[Math.floor(Math.random() * stringContainer.upABC.length)];
-  // }
-  // console.log(pwdOutput.slice(0, inputLength));
-
-
-// var emptyString = "";
-// var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// while (emptyString.length < 6) {
-// emptyString += upperAlphabet[Math.floor(Math.random() * upperAlphabet.length)];
-// } 
-// console.log(emptyString);
-
-// var emptyString = "";
-// var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-
-// while (emptyString.length < 6) {
-// emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
-// } 
-// console.log(emptyString);
-
-// var specialCharString = "";
-// var specialChar = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-
-// while (specialCharString.length < 6) {
-// specialCharString += specialChar[Math.floor(Math.random() * specialChar.length)];
-// } 
-// console.log(specialCharString);
-
-// var numString = "";
-// var numbers = "0123456789";
-
-// while (numString.length < 6) {
-// numString += numbers[Math.floor(Math.random() * numbers.length)];
-// } 
-// console.log(numString);
-
-
-
-
-
-
-  // var secPwd = "";
-
-  // while (secPwd <= inputLength) {
-  //   secPwd += 
-  // }
 } 
 
-generatePassword();
-
-
+// generatePassword();
 
   // Pseudocode
     // Gather user input on password requirements
